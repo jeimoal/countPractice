471a06de6501289bc7c8b8f74dbdb80ddd67293a
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-07-14 13:13:07 -0300

    Update stw.js

--- stw.js
+++ stw.js
@@ -1,5 +1,4 @@
 let ct = {
-btn : null,
 timer : null,
 setZero : 0,
 
@@ -7,6 +6,8 @@ setZero : 0,
 		btn = document.getElementById('btn');
 		btnDec = document.getElementById('btnDec');
 		btnStp = document.getElementById('btnStp');
+		btnRst = document.getElementById('btnRst');
+		myCt = document.getElementById('myCt');
 
 		btn.onclick = ct.inc;
 		btnDec.onclick = ct.decrs;
@@ -15,39 +16,61 @@ setZero : 0,
 	//increasing count
 	inc : () => {
 		ct.timer = setInterval(ct.tickInc, 1000);
+		btn.hidden = true;
 		btnDec.hidden = true;
 		btnStp.hidden = false;
+		btnRst.hidden = false;
 		btnStp.onclick = ct.stop;
+		btnRst.onclick = ct.reset;
 	},
 
 	//decreasing count
 	decrs : () => {
 		ct.timer = setInterval(ct.tickDecrs, 1000);
+		btnDec.hidden = true;
 		btn.hidden = true;
 		btnStp.hidden = false;
+		btnRst.hidden = false;
 		btnStp.onclick = ct.stop;
+		btnRst.onclick = ct.reset;
 	},
 
 	tickInc : () => {
+		ctnue = ct.setZero;
 		ct.setZero++;
-		let show = document.getElementById('demo');
-		let create = document.createElement('p');
-		let pTxt = document.createTextNode(ct.setZero);
+		show = myCt;
+		create = document.createElement('p');
+		pTxt = document.createTextNode(ct.setZero);
 		create.appendChild(pTxt);
 		show.appendChild(create);
 	},
 
 	tickDecrs : () => {
+		ctnue = ct.setZero;
 		ct.setZero--;
-		let show = document.getElementById('demo');
-		let create = document.createElement('p');
-		let pTxt = document.createTextNode(ct.setZero);
+		show = myCt;
+		create = document.createElement('p');
+		pTxt = document.createTextNode(ct.setZero);
 		create.appendChild(pTxt);
 		show.appendChild(create);
 	},
 
 	stop : () => {
 		clearInterval(ct.timer);
+		btn.hidden = false;
+		btnDec.hidden = false;
+		btnStp.hidden = true;
+	},
+
+	reset : () => {
+		clearInterval(ct.timer);
+		clear = myCt;
+		clear.textContent = "";
+		ct.setZero = 0;
+		btn.hidden = false;
+		btnDec.hidden = false;
+		btnStp.hidden = true;
+		btnRst.hidden = true;
 	},
 
 };
